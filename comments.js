fc7bd182450a5db15c66521d37235b5ae53c8cfa
Corey Schomer <coreyschomer@gmail.com>
2024-10-09 20:26:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,9 @@
-// create webserver 
+
+// run the server with node comments.js
+// open browser and navigate to http://localhost:3000/comments
+// you should see the text 'Hello World' show up in the browser
+
+// create webserver
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -8,11 +13,11 @@ app.get('/comments', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/comments/:id', (req, res) => {
+    res.send(`The id is: ${req.params.id}`);
+});
+
 // start the server
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// run the server with node comments.js
-// open browser and navigate to http://localhost:3000/comments
-// you should see the text 'Hello World' show up in the browser
