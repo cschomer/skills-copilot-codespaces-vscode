b70c001db12af8d341e6743abb93c932bac0fbf6
Corey Schomer <coreyschomer@gmail.com>
2024-10-09 20:14:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,43 +1,18 @@
 // create webserver 
-// create a server listening on port 3000
-// create a route that listens to /comments
-// when the route is hit, it should return a json object with the following data
-// { comments: [{ username: 'Todd', comment: 'lol' }, { username: 'Jack', comment: 'ayy lmao' }]}
-// create a route that listens to /form
-// when the route is hit, it should return a form that takes in a username and a comment
-// when the form is submitted, it should make a post request to /comments
-// the post request should have the data { username: [username], comment: [comment] }
-// once the post request is complete, it should redirect back to /form
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/comments', function(req, res) {
-  res.json({
-    comments: [
-      { username: 'Todd', comment: 'lol' },
-      { username: 'Jack', comment: 'ayy lmao' }
-    ]
-  });
+// create a path that will return a response
+app.get('/comments', (req, res) => {
+    res.send('Hello World');
 });
 
-app.get('/form', function(req, res) {
-  res.send(`
-    <form method="post" action="/comments">
-      <input name="username" type="text">
-      <input name="comment" type="text">
-      <input type="submit">
-    </form>
-  `);
+// start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.post('/comments', function(req, res) {
-  console.log(req.body);
-  res.redirect('/form');
-});
-
-app.listen(3000);
+// run the server with node comments.js
+// open browser and navigate to http://localhost:3000/comments
+// you should see the text 'Hello World' show up in the browser
